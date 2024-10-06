23c83da3763797ba09c268ac557e8bbe3335caa2
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-10-06 17:58:56 +0600

    🧼 Update: Handled optional value with match

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,22 @@ use std::env::{args, Args};
 
 fn main() {
     let mut args: Args = args();
-    let first = args.nth(0).unwrap();
 
-    println!("{:?}", first);
+    let first = args.nth(1);
+    match first {
+        Some(inner) => println!("Got first argument: {}", inner),
+        None => panic!("Didn't get the first arguments"),
+    }
+
+    let operator = args.nth(0);
+    match operator {
+        Some(inner) => println!("Got operator: {}", inner),
+        None => panic!("Didn't get the operator"),
+    }
+
+    let second = args.nth(0);
+    match second {
+        Some(inner) => println!("Got second argument: {}", inner),
+        None => panic!("Didn't get the second arguments"),
+    }
 }
