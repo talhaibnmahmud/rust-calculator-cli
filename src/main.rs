d5fe57a1cc51ef70051db0e22a5d688cde67410b
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-10-06 19:05:28 +0600

    🛠 Working: Implemented basic calculation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,57 @@
 use std::env::{args, Args};
 
+fn operate(operator: char, a: f64, b: f64) -> f64 {
+    if operator == '/' {
+        return a / b;
+    };
+    if operator == '*' {
+        return a * b;
+    }
+    if operator == '+' {
+        return a + b;
+    }
+    if operator == '-' {
+        return a - b;
+    }
+
+    return 0.0;
+}
+
+fn outout(a: f64, o: char, b: f64, r: f64) -> String {
+    return format!("{} {} {} = {}", a, o, b, r);
+}
+
 fn main() {
     let mut args: Args = args();
 
     let first = args.nth(1);
     match first {
-        Some(inner) => println!("Got first argument: {}", inner),
+        Some(ref inner) => println!("Got first argument: {}", inner),
         None => panic!("Didn't get the first arguments"),
     }
 
     let operator = args.nth(0);
     match operator {
-        Some(inner) => println!("Got operator: {}", inner),
+        Some(ref inner) => println!("Got operator: {}", inner),
         None => panic!("Didn't get the operator"),
     }
 
     let second = args.nth(0);
     match second {
-        Some(inner) => println!("Got second argument: {}", inner),
+        Some(ref inner) => println!("Got second argument: {}", inner),
         None => panic!("Didn't get the second arguments"),
     }
+
+    println!(
+        "Arguments passed are: {:?}, {:?}, {:?}",
+        first, operator, second
+    );
+
+    let first_number = first.unwrap().parse::<f64>().unwrap();
+    let second_number = second.unwrap().parse::<f64>().unwrap();
+    let operator = operator.unwrap().chars().next().unwrap();
+    println!("Parsed numbers: {:?}, {:?}", first_number, second_number);
+
+    let result = operate(operator, first_number, second_number);
+    println!("{}", outout(first_number, operator, second_number, result));
 }
